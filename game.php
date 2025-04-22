de31e8a487d963f7e4855993f41090d8b917928b
user <user@example.com>
2025-04-22 09:34:47 +0000

    css

--- game.php
+++ game.php
@@ -57,7 +57,7 @@ while($get=mysqli_fetch_array($getData)){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="css/style.css?v=5">
+    <link rel="stylesheet" type="text/css" href="css/style.css?v=6">
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <link href="css/font-awesome.min.css" rel="stylesheet">
